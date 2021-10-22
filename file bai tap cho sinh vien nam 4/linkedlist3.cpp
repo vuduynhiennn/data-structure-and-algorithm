b78c61b843c9f9cdb93488135975c04c629ada57
Vũ Duy Nhiên <88360269+vuduynhiennn@users.noreply.github.com>
2021-10-22 16:20:19 +0700

    add file

--- file bai tap cho sinh vien nam 4/linkedlist3.cpp
+++ file bai tap cho sinh vien nam 4/linkedlist3.cpp
@@ -22,37 +22,6 @@ void insertAtBeginning(struct Node** head_ref, int new_data) {
 }
 
 // Insert a node after a node
-void insertAfter(struct Node* prev_node, int new_data) {
-  if (prev_node == NULL) {
-  cout << "the given previous node cannot be NULL";
-  return;
-  }
-
-  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
-  new_node->data = new_data;
-  new_node->next = prev_node->next;
-  prev_node->next = new_node;
-}
-
-// Insert at the end
-void insertAtEnd(struct Node** head_ref, int new_data) {
-  struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
-  struct Node* last = *head_ref; /* used in step 5*/
-
-  new_node->data = new_data;
-  new_node->next = NULL;
-
-  if (*head_ref == NULL) {
-  *head_ref = new_node;
-  return;
-  }
-
-  while (last->next != NULL) last = last->next;
-
-  last->next = new_node;
-  return;
-}
-
 // Delete a node
 void deleteNode(struct Node** head_ref, int key) {
   struct Node *temp = *head_ref, *prev;
@@ -125,11 +94,13 @@ void printList(struct Node* node) {
 int main() {
   struct Node* head = NULL;
 
-  insertAtEnd(&head, 1);
   insertAtBeginning(&head, 2);
   insertAtBeginning(&head, 3);
-  insertAtEnd(&head, 4);
-  insertAfter(head->next, 5);
+  insertAtBeginning(&head, 3);
+  insertAtBeginning(&head, 3);
+  insertAtBeginning(&head, 3);
+  insertAtBeginning(&head, 3);
+  insertAtBeginning(&head, 3);
 
   cout << "Linked list: ";
   printList(head);
